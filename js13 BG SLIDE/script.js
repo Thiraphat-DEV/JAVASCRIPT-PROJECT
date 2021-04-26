8358c2e4fb5af95752366e447c1b437c00686c36
Thiraphat-DEV <79009861+Thiraphat-DEV@users.noreply.github.com>
2021-04-26 16:02:05 +0700

    JS  11 Background  Slides
    code for life

---
+++ js13 BG SLIDE/script.js
@@ -0,0 +1,48 @@
+
+(()=>{
+    //get Element class  and id button
+    const body  = document.body;
+    const slides = document.querySelectorAll('.slide');
+    const leftBtn = document.getElementById('left');
+    const rightBtn = document.getElementById('right');
+
+    let activeSlider = 0;
+    function onLeft () {
+        activeSlider--;
+
+        if (activeSlider < 0) {
+            activeSlider = slides.length -1 ;
+        }
+        setActive();
+        setBg();
+    }
+    //function button click
+    function onRight() {
+        activeSlider++;
+
+        //check to slides
+        if (activeSlider > slides.length - 1) {
+            activeSlider = 0;
+        }
+
+
+        setBg();
+        setActive();
+    }
+    //setActive();
+    function setActive() {
+        slides.forEach((slide)=>{ //loop foreach
+            slide.classList.remove('active');//remove class
+        });
+        
+        slides[activeSlider].classList.add('active');//add class
+    }
+    //funtion setBg
+    setBg();
+    function setBg() {
+        body.style.backgroundImage = slides[activeSlider].style.backgroundImage;
+    }
+    //add Event click
+    rightBtn.addEventListener('click', onRight);
+    leftBtn.addEventListener('click', onLeft);
+})();
