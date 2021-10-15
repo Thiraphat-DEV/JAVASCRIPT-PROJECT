f7fbede0041c4f262badb6d235fddd4cedc78771
THIRAPHAT B0aty <79009861+Thiraphat-DEV@users.noreply.github.com>
2021-10-15 12:36:55 +0700

    Card Exchange

--- js18 card/card.js
+++ js18 card/card.js
@@ -1,27 +1,27 @@
-(() => {
+
   //get ID
   const header = document.getElementById("header");
   const title = document.getElementById("title");
   const excerpt = document.getElementById("excerpt");
   const profile = document.getElementById("profile-img");
-  const name = document.getElementById("name");
+  const names = document.getElementById("name");
   const dates = document.getElementById("date");
   // get animation  bg and  text
   const animated_bg = document.querySelectorAll(".animated-bg");
   const animated_bg_text = document.querySelectorAll(".animated-bg-text");
 
-
+function random() {
+  return Math.floor(Math.random() * 100);
+}
 //function get data All
   function getDataAll() {
-    header.innerHTML =
-      '<img src="https://images.unsplash.com/photo-1496181133206-80ce9b88a853?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1051&q=80" alt="" />';
+    header.innerHTML ='<img src="https://images.unsplash.com/photo-1496181133206-80ce9b88a853?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1051&q=80" alt="" />';
 
     title.innerHTML = "HELLO MY THIRAPHAT CHORAKHE";
-    excerpt.innerHTML =
-      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eligendi deleniti et ex quidem quos maiores dolores quis cum neque in. Laboriosam fugit molestiae, necessitatibus nesciunt ipsam vel nisi libero error dicta mollitia modi incidunt dolorem repellendus repellat voluptatibus vitae quae.";
-      profile.innerHTML = '<img src="https://randomuser.me/api/portraits/men/90.jpg" alt="">'
+    excerpt.innerHTML ="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eligendi deleniti et ex quidem quos maiores dolores quis cum neque in. Laboriosam fugit molestiae, necessitatibus nesciunt ipsam vel nisi libero error dicta mollitia modi incidunt dolorem repellendus repellat voluptatibus vitae quae.";
+      profile.innerHTML = `<img src="https://randomuser.me/api/portraits/men/${random}.jpg" alt="/">`
 
-      name.innerHTML = 'Thiraphat B.';
+      names.innerHTML = 'Thiraphat B.';
       dates.innerHTML = 'January, 8 2002';
 
       animated_bg.forEach((bg)=>{
@@ -32,12 +32,7 @@
       })
   }
 
-  
-  function run() {
-      
-    setTimeout( getDataAll, 3000);
+    setTimeout( getDataAll, 2000);
 
-  }
+ 
 
-  run();
-})();
