89eb0bdb7fa82adf6813fa040ab9928e2487b4e5
THIRAPHAT B0aty <79009861+Thiraphat-DEV@users.noreply.github.com>
2021-10-18 06:22:17 +0700

    AnimateCount

--- js32AnimationCout/animat.js
+++ js32AnimationCout/animat.js
@@ -3,9 +3,9 @@ const counter = document.querySelector(".counter");
 const finalMessage = document.querySelector(".final");
 const replay = document.querySelector("#replay");
 
-runAnimation();
+runAnimation();//apply animation
 
-function resetDOM() {
+function resetDOM() {//reset number of zero 
   counter.classList.remove("hide");
   finalMessage.classList.remove("show");
 
@@ -13,32 +13,32 @@ function resetDOM() {
     num.classList.value = "";
   });
 
-  nums[0].classList.add("in");
+  nums[0].classList.add("in");//set default zero apply style "in"
 }
 
 function runAnimation() {
-  nums.forEach((num, idx) => {
+  nums.forEach((num, idx) => {//apply foreach and parameter number, index of num
     const nextToLast = nums.length - 1;
 
-    num.addEventListener("animationend", (e) => {
+    num.addEventListener("animationend", (e) => {// require event animation  now is correct target name of animation number
       if (e.animationName === "goIn" && idx !== nextToLast) {
         num.classList.remove("in");
         num.classList.add("out");
       } else if (e.animationName === "goOut" && num.nextElementSibling) {
         num.nextElementSibling.classList.add("in");
       } else {
-        counter.classList.add("hide");
-        finalMessage.classList.add("show");
+        counter.classList.add("hide"); // counter is zero and hide animation
+        finalMessage.classList.add("show"); //apply style show in style.css and apply message from html
       }
     });
   });
 }
 
 function run() {
-  replay.addEventListener("click", () => {
+  replay.addEventListener("click", () => {//apply animation again with click replay word
     resetDOM();
     runAnimation();
   });
 }
 
-run();
+run()
