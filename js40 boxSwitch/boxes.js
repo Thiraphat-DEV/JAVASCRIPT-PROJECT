6b2c43fd55c7bb0e7bac15b874b12502a33465f7
THIRAPHAT B0aty <79009861+Thiraphat-DEV@users.noreply.github.com>
2021-10-19 17:48:22 +0700

    SwitchBoxes

---
+++ js40 boxSwitch/boxes.js
@@ -0,0 +1,75 @@
+//write to code for Life
+window.onload = () => {
+  const boxesContainer = document.querySelector(".box-container");
+  const userImg = document.querySelector(".user-image");
+  const role = document.querySelector(".role");
+  const username = document.querySelector(".username");
+
+  let boxs = document.querySelector(".box");
+
+  //random for object
+  function randomBoxes() {
+    return Math.floor(Math.random() * 100);
+  }
+  //data object fetch in stackOverflow
+  const boxes = [
+    {
+      name: "Miyah Myles",
+      position: "Marketing",
+      photo: `https://randomuser.me/api/portraits/women/${randomBoxes()}.jpg`,
+      text: "I've worked with literally hundreds of HTML/CSS developers and I have to say the top spot goes to this guy. This guy is an amazing developer. He stresses on good, clean code and pays heed to the details. I love developers who respect each and every aspect of a throughly thought out design and do their best to put it in code. He goes over and beyond and transforms ART into PIXELS - without a glitch, every time.",
+    },
+    {
+      name: "June Cha",
+      position: "Software Engineer",
+      photo: `https://randomuser.me/api/portraits/women/${randomBoxes()}.jpg`,
+      text: "This guy is an amazing frontend developer that delivered the task exactly how we need it, do your self a favor and hire him, you will not be disappointed by the work delivered. He will go the extra mile to make sure that you are happy with your project. I will surely work again with him!",
+    },
+    {
+      name: "Iida Niskanen",
+      position: "Data Entry",
+      photo: `https://randomuser.me/api/portraits/women/${randomBoxes()}.jpg`,
+      text: "This guy is a hard worker. Communication was also very good with him and he was very responsive all the time, something not easy to find in many freelancers. We'll definitely repeat with him.",
+    },
+    {
+      name: "Renee Sims",
+      position: "Receptionist",
+      photo: `https://randomuser.me/api/portraits/men/${randomBoxes()}.jpg`,
+      text: "This guy does everything he can to get the job done and done right. This is the second time I've hired him, and I'll hire him again in the future.",
+    },
+    {
+      name: "Jonathan Nunfiez",
+      position: "Graphic Designer",
+      photo: `https://randomuser.me/api/portraits/men/${randomBoxes()}.jpg`,
+      text: "I had my concerns that due to a tight deadline this project can't be done. But this guy proved me wrong not only he delivered an outstanding work but he managed to deliver 1 day prior to the deadline. And when I asked for some revisions he made them in MINUTES. I'm looking forward to work with him again and I totally recommend him. Thanks again!",
+    },
+    {
+      name: "Sasha Ho",
+      position: "Accountant",
+      photo: `https://randomuser.me/api/portraits/men/${randomBoxes()}.jpg`,
+      text: "This guy is a top notch designer and front end developer. He communicates well, works fast and produces quality work. We have been lucky to work with him!",
+    },
+    {
+      name: "Veeti Seppanen",
+      position: "Director",
+      photo: `https://randomuser.me/api/portraits/men/${randomBoxes()}.jpg`,
+      text: "This guy is a young and talented IT professional, proactive and responsible, with a strong work ethic. He is very strong in PSD2HTML conversions and HTML/CSS technology. He is a quick learner, eager to learn new technologies. He is focused and has the good dynamics to achieve due dates and outstanding results.",
+    },
+  ];
+
+  function updateBoxes() {
+    for (let idx = 0; idx <= boxes.length; idx++) {
+      const { name, position, photo, text } = boxes[idx];
+      boxs.innerHTML = text;
+      userImg.src = photo;
+      username.innerHTML = name;
+      role.innerHTML = position;
+    }
+    boxes.forEach(idx => {
+      boxesContainer.append(boxes[idx])
+    })
+  }
+
+  setInterval(updateBoxes(), 1000);
+  //set time
+};
